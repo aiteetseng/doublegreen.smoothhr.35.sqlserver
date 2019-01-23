acdecc81cd66b9931f185942adc2514c7fe122ed
y.c.tseng <46866203+aiteetseng@users.noreply.github.com>
2019-01-23 22:58:02 +0800

    Update authorizedcontent.sql

--- authorizedcontent.sql
+++ authorizedcontent.sql
@@ -1,1 +1,10 @@
-
+select id as authorizedcid,
+       nocode,
+       linkurl,
+       isnull(descriptionzhtw,description) as description, 
+       version
+from dgcontent
+where functioncid is null 
+  and catid is not null 
+  and nocode is not null
+  and browsable = 'Y' order by nocode 
