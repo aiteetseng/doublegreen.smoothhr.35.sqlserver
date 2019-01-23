2bfcc202c4cb8a231138b5d4ec97d231f944e01d
y.c.tseng <46866203+aiteetseng@users.noreply.github.com>
2019-01-23 23:38:48 +0800

    Update grantedcontent.sql

--- dgcontent/grantedcontent.sql
+++ dgcontent/grantedcontent.sql
@@ -7,12 +7,12 @@ select c.id as grantedcid,
   from dgcontent c 
  where c.id in 
       (select a.cid from dgcontentauth a
-       where a.ruid not in 
-            (select ruid from dgcontentauth 
-             where cid='1615E6C2-7803-4F1C-803A-1FC546AE01CB')
-         and a.cid in 
-            (select id from dgcontent
-             where functioncid is null 
-               and catid is not null 
-               and nocode is not null)) order by c.nocode
+        where a.ruid not in 
+             (select ruid from dgcontentauth 
+               where cid='1615E6C2-7803-4F1C-803A-1FC546AE01CB')
+          and a.cid in 
+             (select id from dgcontent
+               where functioncid is null 
+                 and catid is not null 
+                 and nocode is not null)) order by c.nocode
 
