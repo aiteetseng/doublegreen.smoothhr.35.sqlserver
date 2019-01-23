83d0f9fb37cff39a6ca639361d15672462f43af5
y.c.tseng <46866203+aiteetseng@users.noreply.github.com>
2019-01-23 21:56:58 +0800

    Update grantedcontent.sql

--- grantedcontent.sql
+++ grantedcontent.sql
@@ -6,12 +6,12 @@ where functioncid is null
   and catid is not null 
   and nocode is not null
   
-select cid as grantedcid
+select a.cid as grantedcid
 from dgcontentauth a
 where a.ruid not in 
       (select ruid from dgcontentauth 
       where cid='1615E6C2-7803-4F1C-803A-1FC546AE01CB')
-  and cid in 
+  and a.cid in 
       (select id from dgcontent
       where functioncid is null 
       and catid is not null 
